e732b1528de427bd11031fb3123b686b8673d490
Surjendu kar <rahulgotp9988@gmail.com>
2024-07-12 23:12:57 +0530

    ADD eventDetailPage

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,30 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
 import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 
 function EventDetailsPage() {
   const router = useRouter();
-  const getEvents = getEventById(router.query.eventId);
-  console.log(getEvents);
-  return <div>Event Detail</div>;
+  const event = getEventById(router.query.eventId);
+  if (!event) return <p>No event found!</p>;
+
+  return (
+    // handle jsx elements
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.address}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
 export default EventDetailsPage;
