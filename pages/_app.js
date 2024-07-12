435968a3e472fe693854909058b427777ad78adb
Surjendu kar <rahulgotp9988@gmail.com>
2024-07-12 23:24:40 +0530

    ADD NavBar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import Layout from "@/components/layout/layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    //layout is our navbar component
+    <Layout> 
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
